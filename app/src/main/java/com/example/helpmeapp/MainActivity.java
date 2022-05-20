7f24099b1fae5840a9c88fa9e8baa0560b42f425
keny <helpmeapp2022@gmail.com>
2022-05-20 12:16:51 +0400

    3 initial commit

--- app/src/main/java/com/example/helpmeapp/MainActivity.java
+++ app/src/main/java/com/example/helpmeapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is a test
-        //Tushum
+        //keny Poisson
     }
 }
