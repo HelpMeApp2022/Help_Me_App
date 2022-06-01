47edffae3bd0ed865f136a46af2d512223da1e21
keny <helpmeapp2022@gmail.com>
2022-06-01 11:41:46 +0400

    SMS Function Added 01/06

--- app/src/main/java/com/example/helpmeapp/MainActivity.java
+++ app/src/main/java/com/example/helpmeapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.helpmeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,8 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Button trial; //TO DELETE
 
+    //testing sms
+    String phone = "58806717"; //you can put any target phone number here to test
+    String mes = "www.facebook.com";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -50,15 +62,26 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    //Function for sending SMS
+
     public void sendSMS(String phoneNo, String msg) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            //smsManager.sendTextMessage("59215712", null, msg, null, null);
             Toast.makeText(getApplicationContext(), "Message Sent", Toast.LENGTH_LONG).show();
         } catch (Exception ex) {
             Toast.makeText(getApplicationContext(), ex.getMessage().toString(), Toast.LENGTH_LONG).show();
             ex.printStackTrace();
         }
+
+    }
+
+    //function get triggered when sos button is pressed
+
+    public void sos(View view) {
+        sendSMS(phone,mes);
 
     }
 }
