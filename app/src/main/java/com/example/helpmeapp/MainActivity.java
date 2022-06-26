5bbaa28549e481b389488985428ef1aacc1b1078
Ashita <helpmeapp2022@gmail.com>
2022-06-26 19:55:57 +0400

    -Ashita
    Call emergency contacts

--- app/src/main/java/com/example/helpmeapp/MainActivity.java
+++ app/src/main/java/com/example/helpmeapp/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnVoice;
 
     private FirebaseAuth firebaseAuth; //database connection
-    private String textEmergency1, textEmergency2, textEmergency3;
+    private String textEmergency1, textEmergency2, textEmergency3, textRelation1,textRelation2,textRelation3;
     private FusedLocationProviderClient fusedLocationClient;
 
     @Override
@@ -122,8 +122,8 @@ public class MainActivity extends AppCompatActivity {
                 {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
                 ContactsContract.CommonDataKinds.Phone.NUMBER};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        int idxName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        int idxNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+        //int idxName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        //int idxNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
 
         // calling different services
         switch (name){
@@ -143,25 +143,58 @@ public class MainActivity extends AppCompatActivity {
                 callIntent3.setData(Uri.parse("tel:"+114));
                 startActivity(callIntent3);
                 break;
-            default:
-                Toast.makeText(this, "Did not capture! Try-Again!!", Toast.LENGTH_SHORT).show();
-                openDialog();
+//            case "emergency":
+//                Intent callIntent4 = new Intent(Intent.ACTION_CALL);
+//                callIntent4.setData(Uri.parse("tel:"+textEmergency1));
+//                startActivity(callIntent4);
+//                break;
+
+//            default:
+//               Toast.makeText(this, "Did not capture! Try-Again!!", Toast.LENGTH_SHORT).show();
+//                openDialog();
         }
 
         if(cursor.moveToFirst()) {
 
             do {
-               String contactName   = cursor.getString(idxName);
-               String contactNumber = cursor.getString(idxNumber);
+                //String contactName   = cursor.getString(idxName);
+                //String contactNumber = cursor.getString(idxNumber);
 
-                if (contactName.equals(name)){
+               //Fetch details from settings page and associate with variables
+               String contactName1   = textRelation1;
+               String contactNumber1 = textEmergency1;
+               String contactName2   = textRelation2;
+               String contactNumber2 = textEmergency2;
+               String contactName3   = textRelation3;
+               String contactNumber3 = textEmergency3;
+
+
+                if (contactName1.equals(name)){
                     //checking if permission has been granted by user else ask for permission
                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},100);
                     }
-                   //calling the person
+                   //calling the emergency contact 1
                     Intent callIntent = new Intent(Intent.ACTION_CALL);
-                    callIntent.setData(Uri.parse("tel:"+contactNumber));
+                    callIntent.setData(Uri.parse("tel:"+contactNumber1));
+                    startActivity(callIntent);
+                }else if (contactName2.equals(name)){
+                    //checking if permission has been granted by user else ask for permission
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},100);
+                    }
+                    //calling the emergency contact 2
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:"+contactNumber2));
+                    startActivity(callIntent);
+                }else if (contactName3.equals(name)) {
+                    //checking if permission has been granted by user else ask for permission
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 100);
+                    }
+                    //calling the emergency contact 3
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + contactNumber3));
                     startActivity(callIntent);
                 }
             } while (cursor.moveToNext());
@@ -211,6 +244,9 @@ public class MainActivity extends AppCompatActivity {
                     textEmergency1 = userInfo.emergencyContact1;
                     textEmergency2 = userInfo.emergencyContact2;
                     textEmergency3 = userInfo.emergencyContact3;
+                    textRelation1 = userInfo.relation1;
+                    textRelation2 = userInfo.relation2;
+                    textRelation3 = userInfo.relation3;
 
                 } else {
                     Toast.makeText(MainActivity.this, "Something went wrong! Please try once again.", Toast.LENGTH_LONG).show();
